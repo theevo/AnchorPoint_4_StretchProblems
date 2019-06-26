345a9dbdf762f9d6f4670506e812f52d38876c38
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-06-26 14:29:14 -0600

    Adds playground pages with formatted markup

---
+++ AnchorPoint4StretchProblem.playground/Pages/4.3 - Obj-C Highest Number in an Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+/*:
+ # Wednesday Stretch Problem 3.3
+ ## Obj-C Highest Number in Array
+ 
+ ### Instructions
+ 1. Make a new Objective-C Project
+ 2. Write a method that prints the highest number in an array (that is stored as a property).
+ 3. Change your method to take an array as a parameter and return the highest number. Once you do this, you can comment out your property and just make an array to use.
+ 
+ ### Black Diamond 💎💎💎
+ Modify your function so it also prints out the second highest number in the array.
+*/
+
+import Foundation
+
+//: [Next](@next)
---
+++ AnchorPoint4StretchProblem.playground/Pages/4.4 - Obj-C FizzBuzz.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+/*:
+
+ # Thursday Stretch Problem 4.4
+ ## Obj-C FizzBuzz
+ 
+ ### Instructions:
+ 1. Make a new Objective-C Project
+ 2. Write a method that prints the numbers from 1 to an inputed number. But for multiples of three print "Dev" instead of the number and for the multiples of five print "Mtn". For numbers which are multiples of both three and five print "DevMtn".
+ 
+ ### Black Diamond 💎💎💎
+ Make a function in Objective-C that takes one parameter as an integer and returns the closest number in the Fibonacci sequence below the parameter. Then do it again using recursion.
+*/
+
+import Foundation
+
+//: [Next](@next)
